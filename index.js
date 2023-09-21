349bd18dc0d4a42bffc6f937ce3b694dda43d8bb
Pankaj Makhijani <pankajmakhijani6@gmail.com>
2023-09-21 13:41:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    const html = '<html><body><h1>My Application?</h1></body></html>';
+    const html = '<html><body><h1>My Application ?</h1></body></html>';
     res.end(html);
 });
 app.listen(port, () => console.log(`Hello world app started & listening on port ${port}!`))
